0c0053d8572d1fab42c68a81141b10e95bce2c06
Sam Day <me@samcday.com>
2021-07-17 16:49:23 +0200

    create k3s control node

--- index.ts
+++ index.ts
@@ -5,9 +5,31 @@ const network = new hcloud.Network("infra", {
   ipRange: "10.0.0.0/24",
 });
 
+const networkId = network.id.apply(id => parseInt(id, 10));
+
 const subnet = new hcloud.NetworkSubnet("infra", {
-  networkId: network.id.apply(id => parseInt(id, 10)),
+  networkId: networkId,
   networkZone: "eu-central",
   ipRange: "10.0.0.0/24",
   type: "cloud",
+});
+
+const sshKey = new hcloud.SshKey("personal", {
+  publicKey: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFwawprQXEkGl38Q7T0PNseL0vpoyr4TbATMkEaZJTWQ",
+});
+
+const controlNode = new hcloud.Server("control", {
+  image: "debian-10",
+  serverType: "cpx11",
+  location: "nbg1",
+  sshKeys: [sshKey.id],
+  networks: [
+    {
+      ip: "10.0.0.2",
+      networkId,
+    }
+  ],
+  userData: `
+    curl -sfL https://get.k3s.io | sh -
+  `
 });
