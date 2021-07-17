f25e513d9d387c19e8b67640fcf04c66798778ba
Sam Day <me@samcday.com>
2021-07-17 21:35:05 +0200

    setup firewall, install k3s properlyish

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as hcloud from "@pulumi/hcloud";
 
+const cfg = new pulumi.Config();
+
 const network = new hcloud.Network("infra", {
   ipRange: "10.0.0.0/24",
 });
@@ -18,6 +20,20 @@ const sshKey = new hcloud.SshKey("personal", {
   publicKey: "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFwawprQXEkGl38Q7T0PNseL0vpoyr4TbATMkEaZJTWQ",
 });
 
+const firewall = new hcloud.Firewall("infra", {
+  rules: [
+    {
+      direction: "in",
+      protocol: "tcp",
+      port: "22",
+      sourceIps: [
+        "0.0.0.0/0",
+        "::/0",
+      ],
+    },
+  ]
+})
+
 const controlNode = new hcloud.Server("control", {
   image: "debian-10",
   serverType: "cpx11",
@@ -29,7 +45,10 @@ const controlNode = new hcloud.Server("control", {
       networkId,
     }
   ],
-  userData: `
-    curl -sfL https://get.k3s.io | sh -
-  `
+  firewallIds: [firewall.id.apply(id => parseInt(id, 10))],
+  userData: cfg.requireSecret("k3s_token").apply(k3s_token => `#!/bin/bash
+apt update
+apt install apparmor apparmor-utils
+curl -sfL https://get.k3s.io | K3S_TOKEN="${k3s_token}" sh -
+  `),
 });
