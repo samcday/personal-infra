e72ea2a663c2e8a07ffaba633486ffc1980a1135
Sam Day <me@samcday.com>
2021-07-17 16:29:41 +0200

    add subnet

--- index.ts
+++ index.ts
@@ -4,3 +4,10 @@ import * as hcloud from "@pulumi/hcloud";
 const network = new hcloud.Network("infra", {
   ipRange: "10.0.0.0/24",
 });
+
+const subnet = new hcloud.NetworkSubnet("infra", {
+  networkId: network.id.apply(id => parseInt(id, 10)),
+  networkZone: "eu-central",
+  ipRange: "10.0.0.0/24",
+  type: "cloud",
+});
