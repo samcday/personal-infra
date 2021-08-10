62503285bc950663114322a48aed9ec374cef04f
Sam Day <me@samcday.com>
2021-08-10 21:46:13 +0200

    nitter: okay just mostly let Nitter do its thing for now.

--- fly-apps/nitter/entrypoint.sh
+++ fly-apps/nitter/entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
-eval $(/redis_env.py)
 
 sed -i \
-    -e s/__REDISHOST__/$REDIS_HOST/ \
-    -e s/__REDISPORT__/$REDIS_PORT/ \
-    -e s/__REDISPASS__/$REDIS_AUTH/ \
     -e s/__HMAC__/$HMAC/ \
     /src/nitter.conf
 
-/src/nitter
+/src/start.sh
